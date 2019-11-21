19b2808ce4e06c7469d7cc82c02012cd6966cf1e
Gregory Morse <gregory.morse@live.com>
2019-11-21 06:46:00 +0100

    Update test_lad.py

--- lad/tests/test_lad.py
+++ lad/tests/test_lad.py
@@ -15,12 +15,10 @@ def data():
 def test_template_classifier(data):
     X, y = data
     clf = LADClassifier()
-    assert clf.demo_param == 'demo'
 
     clf.fit(X, y)
     assert hasattr(clf, 'classes_')
-    assert hasattr(clf, 'X_')
-    assert hasattr(clf, 'y_')
+    assert hasattr(clf, 'booleqs_')
 
     y_pred = clf.predict(X)
     assert y_pred.shape == (X.shape[0],)
